97a1a31216e8955e23e6cf9cb041deb6b94be6ec
JackBosco <Boscoj24@mail.wlu.edu>
2025-11-19 18:01:44 -0500

    fix: expect n-1 capacity in unit test

--- test/abstract_queue_tests.cpp
+++ test/abstract_queue_tests.cpp
@@ -2,37 +2,36 @@
 #include <lockedin/spsc_queue.hpp>
 
 #include <cassert>
-#include <optional>
-#include <string>
-#include <vector>
+#include <iostream>
 
 template <class Q>
 requires lockedin::detail::QueueInterface<Q, int>
-void unitTest(Q& q, const int fillLimit)
+void unitTest(Q& q)
 {
+    const int fillLimit = 3;
     assert(q.empty());
     assert(!q.full());
 
     assert(q.push(1));
     assert(q.push(2));
     assert(q.push(3));
-    assert(q.push(4));
     assert(q.full());
     assert(!q.push(10));
 
     int popped = 0;
-    assert(q.pop(popped) && popped == 0);
     assert(q.pop(popped) && popped == 1);
+    assert(q.pop(popped) && popped == 2);
     assert(q.size() == static_cast<size_t>(fillLimit - 2));
 
     assert(q.push(11));
     assert(q.size() == static_cast<size_t>(fillLimit - 1));
+    std::cout << "[PASSED]" << "\n";
 }
 
 int main()
 {
     lockedin::SPSCQ<int> stub{4};
-    unitTest(stub, 4);
+    unitTest(stub);
 
     return 0;
 }
